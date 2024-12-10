74d8414566b622260aa0d9c4de158076fdf4282e
SheidaMohassesy <sheida.mohassesy@gmail.com>
2024-12-10 22:14:21 +0100

    Toggle betweet layout

--- src/pages/components/ContentContainer/ContentContainer.jsx
+++ src/pages/components/ContentContainer/ContentContainer.jsx
@@ -1,7 +1,16 @@
+import { useAppContext } from "@/contexts/AppContext";
+import ToggleList from "../ToggleList/ToggleList";
 import styles from "./ContentContainer.module.scss";
 
 const ContentContainer = () => {
-  return <div>content container</div>;
+  const { isListSelected } = useAppContext();
+  return (
+    <div className={styles.contentContainer}>
+      <ToggleList />
+      {isListSelected && <div> list view</div>}
+      {!isListSelected && <div> grid view</div>}
+    </div>
+  );
 };
 
 export default ContentContainer;
